21da958c54e95661c819e3c0b359ad9e2b3e0f07
JordiVives <jvives2@milaifontanals.org>
2021-04-21 19:23:58 +0200

    push

---
+++ HolaMon/src/holamon/HolaMon.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holamon;
+
+/**
+ *
+ * @author vives
+ */
+public class HolaMon {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String s="hola món";
+        System.out.println(s);
+    }
+    
+}
